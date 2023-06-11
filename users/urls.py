f114766ee6d362cf871d7728a4e68d14d3b9bc28
Nonya Johnson III, Esquire <nonya@business.com>
2023-06-11 05:01:15 -0500

    urls

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 
 from .views import UserView
 
 urlpatterns = [
-    path('profile/',  login_required(UserView.as_view()), name='profile'),
+    path('',  login_required(UserView.as_view()), name='profile'),
 ]
